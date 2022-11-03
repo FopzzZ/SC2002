abca5c692ab2a7917315c6a42ebaae9ad7ac1041
yanrenyu0 <yanrenyu00@gmail.com>
2022-11-03 19:09:18 +0800

    edit a bit

--- src/Boundary/AdminLoginUI.java
+++ src/Boundary/AdminLoginUI.java
@@ -1,25 +1,27 @@
 package Boundary;
 
+import Controller.InputController;
+
 public class AdminLoginUI {
-    private String username; //decide on userID or email
+    private String username; // decide on userID or email
     private String password;
-    
+
     public boolean main() {
         boolean result;
         boolean exit = false;
 
-        do{
+        do {
             result = verify();
-            if(!result){
+            if (!result) {
                 System.out.println("Invalid username or password.");
                 System.out.println("1. Enter again");
                 System.out.println("2. Exit");
-                if(InputController.getIntFromUser()==2){
+                if (InputController.getIntFromUser() == 2) {
                     exit = true;
                 }
             }
-        } while (!result&&!exit);
-        if(result){
+        } while (!result && !exit);
+        if (result) {
             System.out.println("You have logged in sucessfully.\n\n");
             return true;
         }
@@ -28,14 +30,15 @@ public class AdminLoginUI {
 
     public boolean verify() {
         System.out.println("Please enter your email: ");
-        username = InputController.getStringFromUser(); //get username
+        username = InputController.getStringFromUser(); // get username
         System.out.println("Password: ");
-        password = InputController.getStringFromUser(); //get password
+        password = InputController.getStringFromUser(); // get password
 
-        if(username == null){ //get rid of empty username (invalid input)
-            return false; 
+        if (username == null) { // get rid of empty username (invalid input)
+            return false;
+        } else {
+            // to be implemented, must check if username exist and if yes, then see if
+            // password match
         }
-        else{
-            // to be implemented, must check if username exist and if yes, then see if password match
     }
 }
