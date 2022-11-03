f927d7f3cb173af2a7902a3a904fb809c097c9a4
yanrenyu0 <yanrenyu00@gmail.com>
2022-11-03 16:15:12 +0800

    Revert "rename"
    This reverts commit 514038f38b4697be3caf9846bd3ecb17f91ca750.

--- Boundary/MainUI.java
+++
@@ -1,5 +0,0 @@
-package Boundary;
-
-public class MainUI {
-
-}
--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 package SC2002;
-
 import java.util.Scanner;
 
 import SC2002.Controller.MainController;
 
 public class Main {
     public static final Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
         MainController system = new MainController();
         system.Work();
