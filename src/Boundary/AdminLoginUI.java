41a5a7d58aa6590771e250dba056aff77ea0c07e
rtao9 <107674111+rtao9@users.noreply.github.com>
2022-11-03 18:33:49 +0800

    Update AdminLoginUI.java

--- src/Boundary/AdminLoginUI.java
+++ src/Boundary/AdminLoginUI.java
@@ -1,7 +1,41 @@
 package Boundary;
 
 public class AdminLoginUI {
-    public void main() {
+    private String username; //decide on userID or email
+    private String password;
+    
+    public boolean main() {
+        boolean result;
+        boolean exit = false;
 
+        do{
+            result = verify();
+            if(!result){
+                System.out.println("Invalid username or password.");
+                System.out.println("1. Enter again");
+                System.out.println("2. Exit");
+                if(InputController.getIntFromUser()==2){
+                    exit = true;
+                }
+            }
+        } while (!result&&!exit);
+        if(result){
+            System.out.println("You have logged in sucessfully.\n\n");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verify() {
+        System.out.println("Please enter your email: ");
+        username = InputController.getStringFromUser(); //get username
+        System.out.println("Password: ");
+        password = InputController.getStringFromUser(); //get password
+
+        if(username == null){ //get rid of empty username (invalid input)
+            return false; 
+        }
+        else{
+            // to be implemented, must check if username exist and if yes, then see if password match
     }
 }
