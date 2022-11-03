514038f38b4697be3caf9846bd3ecb17f91ca750
yanrenyu0 <yanrenyu00@gmail.com>
2022-11-03 16:12:16 +0800

    rename

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 package SC2002;
+
 import java.util.Scanner;
 
 import SC2002.Controller.MainController;
 
 public class Main {
     public static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         MainController system = new MainController();
         system.Work();
