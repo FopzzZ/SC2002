aa8c3fbefffd551a33b9ec3af1468c4228d652f9
yanrenyu0 <yanrenyu00@gmail.com>
2022-11-03 20:51:47 +0800

    login

--- src/Controller/AdminLoginController.java
+++ src/Controller/AdminLoginController.java
@@ -1,7 +1,17 @@
 package Controller;
 
 public class AdminLoginController {
+    String username;
+
+    public AdminLoginController(String username) {
+        this.username = username;
+    }
+
     public boolean verify(String username) {
+        return false;
+    }
+
+    public boolean checkValidUsername() {
         return false;
     }
 }
