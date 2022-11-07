8fae0d20d5609d39706cda19eb7b2b4534992048
UnicornPresident <48112353+UnicornPresident@users.noreply.github.com>
2022-11-07 16:33:46 +0800

    Create EmailCheckController.java

---
+++ src/Controller/EmailCheckController.java
@@ -0,0 +1,43 @@
+package Controller;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.*;
+
+public class EmailCheckController {
+    public static boolean isValid(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                            "[a-zA-Z0-9_+&*-]+)*@" +
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                            "A-Z]{2,7}$";
+        Pattern emailPattern = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return emailPattern.matcher(email).matches();
+    }
+    
+    public static void main(String[] args) {
+        //For testing
+        ArrayList<String> emailAddress = new ArrayList<String>();
+        
+        emailAddress.add("bob@gmail.com");
+        emailAddress.add("john@gmail..com");
+        emailAddress.add("@gmail.com");
+        emailAddress.add("a.b.c@gmail.org");
+        emailAddress.add("bryan@hotmail");
+        
+        for(String address : emailAddress) {
+          if(isValid(address)) {
+              System.out.println("Valid");
+          }
+          else {
+              System.out.println("Invalid");
+          }
+        }
+      
+    }
+  
+}
+
+
+
