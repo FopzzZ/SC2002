25799ac5fadc571ef7ccb524370bc4a976050828
UnicornPresident <48112353+UnicornPresident@users.noreply.github.com>
2022-11-11 17:53:01 +0800

    Added javadoc comments

--- src/Entity/Cinema/CinemaType.java
+++ src/Entity/Cinema/CinemaType.java
@@ -1,5 +1,7 @@
 package Entity.Cinema;
-
+/**
+* CinemaType is an entity
+*/
 public enum CinemaType {
     GOLDCLASS("Gold Class"),
     PLATINUM("Platinum"),
@@ -8,10 +10,19 @@ public enum CinemaType {
 
     private final String text;
 
+    /**
+    * A constructor for the CinemaType Class
+    * 
+    * @param text Determines the type of the cinema
+    */
     private CinemaType(String text) {
         this.text = text;
     }
-
+    /**
+    * A method to convert the CinemaType to a String
+    * 
+    * @return String Type of the cinema
+    */
     public String toString() {
         return this.text;
     }
