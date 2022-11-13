b851dc88593afbcc280efdc27d781bc28b8691e5
UnicornPresident <48112353+UnicornPresident@users.noreply.github.com>
2022-11-13 15:57:53 +0800

    Update ViewMovieDetailsUI.java

--- src/Boundary/ViewMovieDetailsUI.java
+++ src/Boundary/ViewMovieDetailsUI.java
@@ -3,13 +3,16 @@ package Boundary;
 import Controller.InputController;
 import Controller.MovieController;
 
+/**
+* ViewMovieDetailsUI is an entity containing the user interface for viewing movie details
+*/
 public class ViewMovieDetailsUI {
     MovieController movieController = new MovieController();
 
     public void main() {
         movieController.listMovies();
         System.out.println("Select movie to view details: (Enter ID)");
-        int ID = InputController.getIntFromUser(1, movieController.getlistMovies().size());
+        int ID = InputController.getIntFromUser();
         movieController.showDetailByID(ID);
     }
 }
